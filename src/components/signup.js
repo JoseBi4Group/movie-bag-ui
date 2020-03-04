86e80a07d61dbeff90b9d53735c13ebaa404452a
JoseBi4Group <jm.roman@bi4group.com>
2020-03-04 15:30:02 +0100

    add register user functionality

--- src/components/signup.js
+++ src/components/signup.js
@@ -10,40 +10,62 @@ import {
 class Signup extends Component {
   constructor() {
     super();
-    // this.state = {
-    //   users: []
-    // };  
+    this.state={
+      email: '',
+      password: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);  
   }
 
-  // componentDidMount() {
-  //   fetch('http://localhost:5000/users')
-  //   .then(res => res.json())
-  //   .then((data) => {
-  //     this.setState({ users: data })
-  //   })
-  //   .catch(console.log)
-  // }
+  handleChange(event){
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    let data = {
+      email: this.state.email,
+      password: this.state.password
+    };
+
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json())
+    .catch(error => console.error('Error:', error))
+    .then(response => console.log('Success:', response));
+  }
 
   render () {
     return (
       <Container>
-        <center><h1>Log in</h1></center>
-        <Form>
+        <center><h1>Register</h1></center>
+        <Form onSubmit={this.handleSubmit}>
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control type="email" name="email" placeholder="Enter email" value={this.state.email} onChange={this.handleChange} />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
           </Form.Group>
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleChange} />
           </Form.Group>
-          <Form.Group controlId="formBasicPassword">
+          {/* <Form.Group controlId="formBasicPasswordConfirmation">
             <Form.Label>Password Confirmation</Form.Label>
             <Form.Control type="password" placeholder="Password again" />
-          </Form.Group>
+          </Form.Group> */}
           <Button variant="primary" type="submit">
             Submit
           </Button>
